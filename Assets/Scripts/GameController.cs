b1bf59ce2f30f274c0ba708f09afa5e5d25a1ae1
Hina_Sorasaki_763 <hinasorasaki763@gmail.com>
2024-09-01 13:12:44 +0800

    day3_final commit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour
     public MapBuilder mapBuilder;
     void Start()
     {
-        mapBuilder.BuildHollowSquare();
+        //mapBuilder.BuildHollowSquare();
         for (int i = 0; i < 7; i++)
         {
             Vector3 v = new Vector3(0,5+i,0);
